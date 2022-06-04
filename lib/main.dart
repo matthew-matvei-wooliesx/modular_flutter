4fb387b3c8ed83289f4947a1fc1389ba6b20ee7f
Matthew James <mjames4@woolworths.com.au>
2022-06-04 15:21:10 +1000

    Make driver_breaks expectations

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
