9b34df8ac2ce91d46d50517ca507d419eb446ea1
Matthew James <mjames4@woolworths.com.au>
2022-06-17 10:48:59 +1000

    Revert "Move notifier to application layer"
    This reverts commit 38ad49e0d393d29d2c5f72cc1c3389cb1663d448.

--- application/lib/providers.dart
+++ application/lib/providers.dart
@@ -1,15 +1,8 @@
-import 'package:application/driver_break_state_notifier.dart';
 import 'package:data/in_memory_driver_break_state_repository.dart';
 import 'package:domain/driver_break_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final driverBreakStateNotifierProvider = ChangeNotifierProvider(
-  (ref) => DriverBreakStateNotifier(
-    state: ref.read(_driverBreakStateProvider),
-  ),
-);
-
-final _driverBreakStateProvider = Provider(
+final driverBreakStateProvider = Provider(
   (ref) => DriverBreakState(
     repository: ref.read(_driverBreakStateRepositoryProvider),
   ),
