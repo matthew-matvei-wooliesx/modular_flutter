0bdd62d843ea3907a498f1c6bfd03f44a2bcac9b
Matthew James <mjames4@woolworths.com.au>
2022-06-17 10:27:01 +1000

    Move main widgets to presentation layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:presentation/driver_breaks_view.dart';
+import 'package:presentation/app.dart';
 
 void main() async {
   runApp(ProviderScope(child: const App()));
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: DriverBreaksView(),
-    );
-  }
 }
